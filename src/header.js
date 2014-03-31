dac34e4ff63e79220d12169f2e61f7add21cb1fb
Charlie Liang Yuan <charlie@betable.com>
2014-03-30 21:17:30 -0700

    browserify sparkline
    add version
    
    fix typo
    
    add jquery
    
    add jquery as dep so browserify can find it
    
    fix semicolon
    
    fix dist

--- src/header.js
+++ src/header.js
@@ -202,7 +202,9 @@
 
 /*jslint regexp: true, browser: true, jquery: true, white: true, nomen: false, plusplus: false, maxerr: 500, indent: 4 */
 
-(function(document, Math, undefined) { // performance/minified-size optimization
+var jQuery = require('jquery')
+
+;(function(document, Math, undefined) { // performance/minified-size optimization
 (function(factory) {
     if(typeof define === 'function' && define.amd) {
         define(['jquery'], factory);
